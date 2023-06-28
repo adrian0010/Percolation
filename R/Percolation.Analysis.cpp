a0681e6b922610e118be250627b97458351e600c
adrian0010 <adrianivan0010@gmail.com>
2023-06-28 10:36:31 +0300

    Update Percolation.Analysis.cpp

--- R/Percolation.Analysis.cpp
+++ R/Percolation.Analysis.cpp
@@ -1,14 +1,4 @@
-// Student: Adrian Ivan
-// Candidate BSc CS 2023
-// West University, Timisoara
 
-// Supervisors:
-// Prof. Dr. Daniela Zaharie
-// Dr. med. Leonard Mada (Syonic SRL)
-
-// URL: https://github.com/adrian0010/Percolation
-
-// Based on Previous Projects (2020-2022)
 
 NumericVector heightChannel(IntegerVector xStart, NumericVector m,
 		const int nRows, const int nCols,
